2ddd3469fa9705e78cc2059d29f280429f6719b8
Kristinita <vspobog@gmail.com>
2022-02-18 17:36:14 +0300

    test(appveyor): move installation scripts to the file

---
+++ appveyor-install.bat
@@ -0,0 +1,1 @@
+START /B CMD /C "pip install pipenv & pipenv install --dev & pipenv run peru sync"
---
+++ appveyor-pathed.bat
@@ -0,0 +1,1 @@
+START /B CMD /C "choco install gtools -y & refreshenv & pathed /APPEND %PYTHON% /USER & pathed /APPEND %PYTHON%\\Scripts /USER"
