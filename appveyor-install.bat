3215402b3ccea366903a54ad962211c62c0378b0
Kristinita <vspobog@gmail.com>
2022-02-18 17:59:47 +0300

    test(appveyor): try more installations in batch file

--- appveyor-install.bat
+++ appveyor-install.bat
@@ -1,1 +1,3 @@
+START /B CMD /C "choco install html-tidy -y -ignoredependencies"
 START /B CMD /C "pip install pipenv & pipenv install --dev & pipenv run peru sync"
+START /B CMD /C "choco install nodejs phantomjs -y & npm install -g grunt-cli & npm install"
