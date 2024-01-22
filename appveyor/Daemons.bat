20c39a291e562ddcb5b8327b20df2ae8852aa23f
Kristinita <Kristinita@users.noreply.github.com>
2024-01-22 22:08:14 +0300

    ci(appveyor): try START /B CMD /K

--- appveyor/Daemons.bat
+++ appveyor/Daemons.bat
@@ -1,5 +1,5 @@
 @REM @Author: SashaChernykh
 @REM @Date:   2024-01-22 16:01:31
 @REM @Last Modified by:   SashaChernykh
-@REM Modified time: 2024-01-22 22:01:29
-START /wait http-server --port 4147
+@REM Modified time: 2024-01-22 22:06:46
+START /B CMD /K http-server --port 4147
