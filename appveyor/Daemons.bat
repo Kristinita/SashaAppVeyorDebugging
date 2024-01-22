0ebff5f1c806fe3f526a70352802b807a6e0ce44
Kristinita <Kristinita@users.noreply.github.com>
2024-01-22 16:24:10 +0300

    ci(appveyor): try run background server from batch file

---
+++ appveyor/Daemons.bat
@@ -0,0 +1,5 @@
+@REM @Author: SashaChernykh
+@REM @Date:   2024-01-22 16:01:31
+@REM @Last Modified by:   SashaChernykh
+@REM Modified time: 2024-01-22 16:22:10
+START /B CMD /C "http-server --port 4147"
