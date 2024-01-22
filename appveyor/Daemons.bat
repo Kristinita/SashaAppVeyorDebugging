0054981208b45f8d66fdcab376458bc8ebd5c656
Kristinita <Kristinita@users.noreply.github.com>
2024-01-22 19:19:05 +0300

    ci(appveyor): try cmd /c

--- appveyor/Daemons.bat
+++ appveyor/Daemons.bat
@@ -1,5 +1,5 @@
 @REM @Author: SashaChernykh
 @REM @Date:   2024-01-22 16:01:31
 @REM @Last Modified by:   SashaChernykh
-@REM Modified time: 2024-01-22 19:07:34
-START /B "" CMD /C "http-server --port 4147"
+@REM Modified time: 2024-01-22 19:18:35
+CMD /C "http-server --port 4147"
