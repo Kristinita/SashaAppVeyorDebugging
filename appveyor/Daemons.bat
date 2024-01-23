2b52820241c06bbad88a7ccb07545cdea1fe1d66
Kristinita <Kristinita@users.noreply.github.com>
2024-01-23 10:40:28 +0300

    ci(appveyor): try start ''
    https://stackoverflow.com/a/12848306/5951529

--- appveyor/Daemons.bat
+++ appveyor/Daemons.bat
@@ -1,7 +1,7 @@
 @REM @Author: SashaChernykh
 @REM @Date:   2024-01-22 16:01:31
 @REM @Last Modified by:   SashaChernykh
-@REM Modified time: 2024-01-23 08:47:42
-rem START /B CMD /K http-server --port 4147
-powershell -Command "Start-Process cmd -ArgumentList '/c http-server --port 4147'"
-rem START http-server --port 4147
+@REM Modified time: 2024-01-23 10:38:56
+@rem START /B CMD /K http-server --port 4147
+@rem powershell -Command "Start-Process cmd -ArgumentList '/c http-server --port 4147'"
+START "" http-server --port 4147
