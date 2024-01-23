530371eba234c5b2ecdc26a5649cd89a2ad46812
Kristinita <Kristinita@users.noreply.github.com>
2024-01-23 08:09:27 +0300

    ci(appveyor): try run PowerShell command from cmd

--- appveyor/Daemons.bat
+++ appveyor/Daemons.bat
@@ -1,5 +1,7 @@
 @REM @Author: SashaChernykh
 @REM @Date:   2024-01-22 16:01:31
 @REM @Last Modified by:   SashaChernykh
-@REM Modified time: 2024-01-22 22:06:46
-START /B CMD /K http-server --port 4147
+@REM Modified time: 2024-01-22 22:47:50
+rem START /B CMD /K http-server --port 4147
+rem powershell -Command "Start-Process cmd -ArgumentList '/c http-server --port 4147'"
+START http-server --port 4147
