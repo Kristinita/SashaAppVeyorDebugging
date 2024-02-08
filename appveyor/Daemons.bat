4b81381f58f8916b2abe2643ada91bcbbaef0636
Kristinita <Kristinita@users.noreply.github.com>
2024-02-08 10:25:42 +0300

    ci(appveyor): try mkcert with real testing tools

--- appveyor/Daemons.bat
+++
@@ -1,7 +0,0 @@
-@REM @Author: SashaChernykh
-@REM @Date:   2024-01-22 16:01:31
-@REM @Last Modified by:   SashaChernykh
-@REM Modified time: 2024-01-23 10:38:56
-@rem START /B CMD /K http-server --port 4147
-@rem powershell -Command "Start-Process cmd -ArgumentList '/c http-server --port 4147'"
-START "" http-server --port 4147
